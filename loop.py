1e244050e35157921fc6bcaed2cae7e428e262c1
memanesavita <82706660+memanesavita@users.noreply.github.com>
2021-10-07 11:22:52 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,61 @@
+
+
+# i=1
+# sum=0
+# while i<10:
+#     a=int(input("enter the numbr"))
+#     sum=sum+i
+#     print(sum)
+#     i=i+1
+
+# i=1
+# sum=0
+# while i<10:
+#     b=int(input("enter teh number"))
+#     sum=sum+i
+#     print(sum)
+#     if b>0:
+#         print("it is a parfect number")
+#     else:
+#         print("it is a not parfect number")
+#     i=i+1
+
+# i=1
+# sum=0
+# while i<10:
+#     x=int(input("enter the number"))
+#     sum=sum+i
+#     print(sum)
+#     if sum%2==0:
+#         print("it is a even number")
+#     else:
+#         print("it is not even number")
+#     i=i+1
+
+
+# k=int(input("enter the number"))
+# i=0
+# while i<10:
+#     print(i,"x",i,"=",i*k)
+#     i=i+1
+
+# user=int(input("enter the number"))
+# i=1
+# count=0
+# while i<=user:
+#     if user%i==0:
+#         count=count+1
+#     i=i+1
+# if count==2:
+#     print("it is prime number")  
+# else:
+#     print("it is not prim number")
+
+
+
+
+
+
+
+
+
